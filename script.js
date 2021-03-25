a2d559f2b58b900ce3a7b3d28f710848d9e2271a
Wesley Jr <wesleyjr2002@gmail.com>
2021-03-24 22:58:25 -0300

    Added Auto Dark Mode

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function load() {
     var perfil = document.querySelector('img#w')
-    var clock = document.querySelector('p#clock')
     var date = new Date()
-    var hours = date.getHours()
-    clock.innerHTML = hours
+    var hours = date.getHours
+    // Auto Dark Mode System.
+    if (hours > 18 && hours > 6) {
+        document.body.style.backgroundColor = "#0a0a0a"
+        document.body.style.color = "#ffffff"
+        perfil.src = "perfil_dark.png"
+    } else {
+        document.body.style.backgroundColor = "#ffffff"
+        document.body.style.color = "#0a0a0a"
+        perfil.src = "perfil_white.png"
+    }
 }
