5bfd76d4f4b154fc05da5c0185d96bf4ae6e2dfe
Wesley Jr <wesleyjr2002@gmail.com>
2021-04-01 00:21:34 -0300

    Trying to fix a bug

--- _js/script.js
+++ _js/script.js
@@ -10,16 +10,16 @@ function load() {
     if (hours > 18 || hours < 6) {
         document.body.style.backgroundColor = "#0a0a0a"
         document.body.style.color = "#ffffff"
-        perfil.src = "/_img/perfil_white.png"
-        github.src = "/_img/github_white.png"
-        twitter.src = "/_img/twitter_white.png"
-        facebook.src = "/_img/facebook_white.png"
+        perfil.src = "_img/perfil_white.png"
+        github.src = "_img/github_white.png"
+        twitter.src = "_img/twitter_white.png"
+        facebook.src = "_img/facebook_white.png"
     } else {
         document.body.style.backgroundColor = "#ffffff"
         document.body.style.color = "#0a0a0a"
-        perfil.src = "/_img/perfil_black.png"
-        github.src = "/_img/github_black.png"
-        twitter.src = "/_img/twitter_black.png"
-        facebook.src = "/_img/facebook_black.png"
+        perfil.src = "_img/perfil_black.png"
+        github.src = "_img/github_black.png"
+        twitter.src = "_img/twitter_black.png"
+        facebook.src = "_img/facebook_black.png"
     }
 }
