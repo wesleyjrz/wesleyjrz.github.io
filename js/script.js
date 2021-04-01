1641db3d20cab55a01b46cf42db1bf6c295f8e8f
Wesley Jr <wesleyjr2002@gmail.com>
2021-04-01 20:05:45 -0300

    Added presentation box

--- js/script.js
+++ js/script.js
@@ -3,23 +3,25 @@ function load() {
     var github = document.querySelector('img#github')
     var twitter = document.querySelector('img#twitter')
     var facebook = document.querySelector('img#facebook')
+    var presentation_box = document.querySelector('article#about-me')
     var date = new Date()
     var hours = date.getHours()
-
-    // Auto Dark Mode System.
-    if (hours > 18 || hours < 6) {
-        document.body.style.backgroundColor = "#0a0a0a"
-        document.body.style.color = "#ffffff"
-        perfil.src = "/img/perfil_white.png"
-        github.src = "/img/github_white.png"
-        twitter.src = "/img/twitter_white.png"
-        facebook.src = "/img/facebook_white.png"
-    } else {
-        document.body.style.backgroundColor = "#ffffff"
-        document.body.style.color = "#0a0a0a"
-        perfil.src = "/img/perfil_black.png"
-        github.src = "/img/github_black.png"
-        twitter.src = "/img/twitter_black.png"
-        facebook.src = "/img/facebook_black.png"
+    
+    if (hours > 18 || hours < 6) { // Night Mode.
+        document.body.style.backgroundColor = '#0a0a0a'
+        document.body.style.color = '#ffffff'
+        perfil.src = '/img/perfil_white.png'
+        github.src = '/img/github_white.png'
+        twitter.src = '/img/twitter_white.png'
+        facebook.src = '/img/facebook_white.png'
+    } else { // Day Mode.
+        document.body.style.backgroundColor = '#ffffff'
+        document.body.style.color = '#0a0a0a'
+        presentation_box.style.backgroundColor = '#d6d6d6'
+        presentation_box.style.borderColor = '#0a0a0a'
+        perfil.src = '/img/perfil_black.png'
+        github.src = '/img/github_black.png'
+        twitter.src = '/img/twitter_black.png'
+        facebook.src = '/img/facebook_black.png'
     }
 }
